d01fd317fa6b05247e92d6030d3f9dfda7e40286
Bunhann <bunhann@gmail.com>
2018-06-16 09:21:41 +0700

    Update Person File

--- app/src/main/java/com/bunhann/gitreview/Person.java
+++ app/src/main/java/com/bunhann/gitreview/Person.java
@@ -2,5 +2,13 @@ package com.bunhann.gitreview;
 
 public class Person {
 
-    
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
